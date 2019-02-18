49967ba520180f62b264183cfc177f40a87c076d
snoopdug <richard.anderson@sitekit.co.uk>
2019-02-18 16:32:08 +0000

    Create Install.ps1

---
+++ Install.ps1
@@ -0,0 +1,16 @@
+#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#Assign Packages to Install
+$Packages = 'nodejs.install',`
+            'jre8',`
+            'dotnetcore-sdk',`
+            'netfx-4.7.1-devpack'
+
+#Install Packages
+ForEach ($PackageName in $Packages)
+{choco install $PackageName -y}
+
+#Reboot
+Restart-Computer
